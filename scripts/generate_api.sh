9237e8618baff42f47bb9d5cc3291fd339ace3ab
Steven Moder <java20131114@gmail.com>
2025-04-08 19:58:24 +0800

    更新生成API脚本，改用npm替代pnpm以提高兼容性

--- scripts/generate_api.sh
+++ scripts/generate_api.sh
@@ -1,7 +1,7 @@
 rm -rf src/services/api
 mkdir -p src/services/api
 
-pnpm run openapi
+npm run openapi
 
 fd -t f . src/services/api -E "typings.d.ts" -x sed -i '1i\import type { API } from "./typings";' {}
 fd -t f . src/services/api -E "typings.d.ts" -x sed -i '/\/\/ @ts-ignore/d' {}
